570cea40a5472d2de53d8f5609cbfd039d7671c4
capDoYeonLee <ghtyu21@naver.com>
2022-09-14 16:12:05 +0900

    ECNV project

---
+++ simple-HRNet/scripts/predict_sports.py
@@ -0,0 +1,14 @@
+import cv2
+import csv
+import os
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.linear_model import LogisticRegression, RidgeClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+
+
+
